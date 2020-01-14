9ddcc081bd30a5a9f31bf7c1cdb854221fd074ff
agollapudi <avaneeshg@qualminds.com>
2020-01-14 18:54:53 +0530

    single php changes

--- wp-content/themes/qualminds/index.php
+++ wp-content/themes/qualminds/index.php
@@ -1,18 +1,21 @@
 <?php get_header(); ?>
-<div class="container-fluid">
+<div class="container">
     <div class="row">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <div class="col-md-12 col-lg-4">
-            <a href="<?php the_permalink(); ?>" class="white-box p-4 mt-4 d-block no-hover">
-                <div class="">
-                    <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="img-fluid w-100" />
-                </div>
-                <div class="p-3 bgcolor-grey" style="min-height: 125px;">
-                    <div class="ft-1_2em font-weight-bold"><?php the_title(); ?></div>
-                    <div class="ft-1em">
-                        <?php foreach(get_the_category() as $category) { ?>
-                        <div><?php echo $category->name; ?></div>
-                        <?php } ?>
+            <a href="<?php the_permalink(); ?>" class="white-box no-hover">
+                <div class="insights-list">
+                    <div class="">
+                        <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="img-fluid w-100" />
+                    </div>
+                    <div class="p-3" style="min-height: 125px;">
+                        
+                        <div class="ft-1em" style="color: #6f85a3;">
+                            <?php foreach(get_the_category() as $category) { ?>
+                            <div><?php echo strtoupper($category->name); ?></div>
+                            <?php } ?>
+                        </div>
+                        <div class="ft-1_2em font-weight-bold"><?php the_title(); ?></div>
                     </div>
                 </div>
             </a>
--- wp-content/themes/qualminds/single.php
+++ wp-content/themes/qualminds/single.php
@@ -1,19 +1,23 @@
 <?php get_header(); ?>
-<div class="container-fluid">
+<div class="container">
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-12">
         <?php
 				// Start the Loop.
 			while ( have_posts() ) :
                 the_post(); ?>
                 <div class="white-box p-4 single-post">
-                <h1 ><?php the_title(); ?></h1>
-                <div class="d-flex">
-                    <div class="mr-2">By <?php the_author(); ?></div>
-                    <div class="mr-2"> | </div>
-                    <div class="mr-2"><?php the_date(); ?></div>
-                    <div class="mr-2"> | </div>
-                    <div class="mr-2"><?php the_category(); ?></div>
+                <h1 class="main-title"><?php the_title(); ?></h1>
+                <div class="d-flex my-4">
+                    <div class="mr-3 color-red text-uppercase"><span class="light-grey">BY</span> <?php the_author(); ?></div>
+                    <div class="mr-3"> | </div>
+                    <div class="mr-3 color-red">
+                            <?php foreach(get_the_category() as $category) { ?>
+                            <a class="no-hover c-pointer" href="<?php echo get_category_link($category) ?>"><?php echo strtoupper($category->name); ?></a>
+                            <?php } ?>
+                    </div>
+                    <div class="mr-3"> | </div>
+                    <div class="mr-3 light-grey text-uppercase"><?php the_date(); ?></div>
                 </div>
                 <div class="content mt-5"><?php the_content(); ?></div>
                 </div>
