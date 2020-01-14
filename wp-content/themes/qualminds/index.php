03f570ea714348938e5c1df9a9e8c89c63f50d11
agollapudi <avaneeshg@qualminds.com>
2020-01-14 15:44:44 +0530

    white box changes

--- wp-content/themes/qualminds/index.php
+++ wp-content/themes/qualminds/index.php
@@ -1,16 +1,19 @@
 <?php get_header(); ?>
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-md-12">
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <div class="white-box p-4 mt-4">
-                <h1>
-                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-                <div><?php the_excerpt(); ?></div>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <div class="col-md-12 col-lg-4">
+            <a href="<?php the_permalink(); ?>" class="white-box p-4 mt-4 d-block no-hover">
+                <div class="">
+                    <img src="https://qualminds.com/images/insights/thumbnail/Importance-of-Software-Architecture-to-Build-Scalable-Solutions-thumb.png" class="img-fluid w-100" />
                 </div>
-                
-            <?php endwhile; endif; ?>
+                <div class="p-3 bgcolor-grey" style="min-height: 125px;">
+                    <div class="ft-1em"><?php the_title(); ?></div>
+                    <div class="ft-1_2em font-weight-bold"><?php the_excerpt(); ?></div>
+                </div>
+            </a>
         </div>
+        <?php endwhile; endif; ?>
     </div>
 </div>
 <?php get_footer(); ?>
