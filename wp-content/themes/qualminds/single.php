f513f2317924415c3de0468965b71f6a48f59fa4
Leela Narasimha <leelanrp@qualminds.com>
2020-01-23 19:47:29 +0530

    Added internal page new design

--- wp-content/themes/qualminds/single.php
+++ wp-content/themes/qualminds/single.php
@@ -1,28 +1,56 @@
 <?php get_header(); ?>
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-md-12">
-        <?php
-				// Start the Loop.
-			while ( have_posts() ) :
+        <div class="col-md-7 offset-md-1">
+            <?php
+            // Start the Loop.
+            while (have_posts()) :
                 the_post(); ?>
+
                 <div class="white-box p-4 single-post">
-                <h1 class="main-title"><?php the_title(); ?></h1>
-                <div class="d-flex my-4">
-                    <div class="mr-3 color-red text-uppercase"><span class="light-grey">BY</span> <?php the_author(); ?></div>
-                    <div class="mr-3"> | </div>
-                    <div class="mr-3 color-red">
-                            <?php foreach(get_the_category() as $category) { ?>
-                            <a class="no-hover c-pointer" href="<?php echo get_category_link($category) ?>"><?php echo strtoupper($category->name); ?></a>
-                            <?php } ?>
+                    <div class="row">
+                        <div class="col-md-12">
+
+
+                            <h1 class="main-title"><?php the_title(); ?></h1>
+
+
+
+                            <div class="d-flex my-4">
+                                <div class="mr-3 color-red text-uppercase"><span class="light-grey">BY</span> <?php the_author(); ?></div>
+                                <div class="mr-3"> | </div>
+                                <div class="mr-3 color-red">
+                                    <?php foreach (get_the_category() as $category) { ?>
+                                        <a class="no-hover c-pointer" href="<?php echo get_category_link($category) ?>"><?php echo strtoupper($category->name); ?></a>
+                                    <?php } ?>
+                                </div>
+                                <div class="mr-3"> | </div>
+                                <div class="mr-3 light-grey text-uppercase"><?php the_date(); ?></div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="mr-3"> | </div>
-                    <div class="mr-3 light-grey text-uppercase"><?php the_date(); ?></div>
+
+                    <div class="row">
+
+                        <div class="col-md-12">
+                            <div>
+
+                                <img src="https://www.netsolutions.com/insights/wp-content/uploads/2019/01/Top-11-eCommerce-trends-for-2020.jpg" class="img-fluid" />
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="row">
+
+                        <div class="col-md-10 offset-md-1">
+                            <div class="content mt-5"><?php the_content(); ?></div>
+                        </div>
+                    </div>
+
                 </div>
-                <div class="content mt-5"><?php the_content(); ?></div>
-                </div>
-           <?php  endwhile; ?>
-       
+            <?php endwhile; ?>
+
         </div>
     </div>
 </div>
